60f33ed853cf4ba3cede8df081282cc3c5b67f6d
Ademir <ademir@debian.local>
2023-02-14 17:00:36 -0300

    consulta api vendas rapida

--- vendas/script.js
+++ vendas/script.js
@@ -1,43 +1,51 @@
-const url = "http://localhost/users:3000"
-
-const searchItem = document.getElementById("searchItem")
+const url = "http://localhost:3000/products";
+const includedItems = document.getElementById("includedItems")
 const buttonSearchItem = document.querySelector("#search")
-const dataItens= document.getElementById("listItens");
+const listItens= document.getElementById("listItens");
+let newItem = {}
+const soldItems = [] /**Itens Vendidos */
+getItem()
 
-const vendidos = []
-
-/*Itens **/
-const itens = [
-            {id:1, name:"Mouse Serial", barras:"123", valor:18.00},
-            {id:2, name:"Teclado 2", barras:"1234", valor:29.90},
-            {id:3, name:"Monitor AOC 18", barras:"12345", valor:680.90},
-            {id:4, name:"Caixa Som", barras:"123456", valor:120.90}
-            ]
-    itens.map((val)=>{
-            dataItens.innerHTML+=`
-            <option value = "${val.name}"></option>`
-            })
+async function getItem(){
+   await axios.get(url)
+    .then(response =>{
+        const data = response.data
+        const itens = data
+        itens.map((val)=>{
+        listItens.innerHTML+=`
+        <option value = "${val.descric_product}"></option>`
+        })
 
 buttonSearchItem.addEventListener("click", function(e){
-e.preventDefault()
-/* Pegar Input do Value*/
-const item = document.getElementById("item").value
-const quant = document.getElementById("quant").value
-/*Laço for para buscar item*/
-let itens_ = {}
-for(let i=0; itens.length > i; i++)
-    if(item == itens[i].name || item == itens[i].id || item == itens[i].barras){
-        itens_.item = itens[i].name
-        let valor = itens[i].valor
-        itens_.valor = parseFloat(valor).toFixed(2)
-        itens_.totalItem = 0
-        let totalItem_ = itens[i].valor * quant;
-        itens_.totalItem = parseFloat(totalItem_).toFixed(2)
-        /*Rederiza na tela**/
-searchItem.innerHTML +=`
-    <p>${itens_.item} ${itens_.valor} ${itens_.totalItem}</p>`
-    console.log(itens_) 
-    vendidos.push(itens_)  
-    console.log(vendidos)
+    e.preventDefault()
+    /* Pegar Input do Value*/
+        const item = document.getElementById("item").value
+        const quant = document.getElementById("quant").value
+    /*Laço for para buscar item*/
+        for(let i=0; itens.length > i; i++)
+            if (item == itens[i].descric_product || item == itens[i].id_product || item == itens[i].bar_code ){
+                newItem.item = itens[i].descric_product
+                newItem.quant = quant
+                let valor = itens[i].val_max_product
+                newItem.valor = parseFloat(valor).toFixed(2)
+                let totalItem_ = 0
+                totalItem_ = itens[i].val_max_product * quant;
+                newItem.totalItem = parseFloat(totalItem_).toFixed(2)
+    /*Rederiza na tela**/
+includedItems.innerHTML +=`
+        <div id="">
+            <span>${newItem.item}</span>
+            <span>${newItem.quant}</span>
+            <span>${newItem.valor}</span>
+            <span>${newItem.totalItem}</span>
+            <button onClick="">Apagar</button>
+        </ul>`
+    //console.log(newItem) 
+            soldItems.push(newItem)  
+            console.log(soldItems)
+        }})
+    })
+.catch(error =>console.log(error))
 }
-})
+
+
