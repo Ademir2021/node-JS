967a78b95d429260b9018f3c4ddfa2fd9115b7ce
Ademir <ademir@debian.local>
2023-02-15 16:07:44 -0300

    test remove item

--- vendas/script.js
+++ vendas/script.js
@@ -2,9 +2,11 @@ const url = "http://localhost:3000/products";
 const includedItems = document.getElementById("includedItems")
 const buttonSearchItem = document.querySelector("#search")
 const listItens= document.getElementById("listItens");
-let newItem = {}
 const soldItems = [] /**Itens Vendidos */
+
+
 getItem()
+
 
 async function getItem(){
    await axios.get(url)
@@ -18,12 +20,11 @@ async function getItem(){
 
 buttonSearchItem.addEventListener("click", function(e){
     e.preventDefault()
-    /* Pegar Input do Value*/
         const item = document.getElementById("item").value
         const quant = document.getElementById("quant").value
-    /*Laço for para buscar item*/
         for(let i=0; itens.length > i; i++)
             if (item == itens[i].descric_product || item == itens[i].id_product || item == itens[i].bar_code ){
+                let newItem = {}
                 newItem.item = itens[i].descric_product
                 newItem.quant = quant
                 let valor = itens[i].val_max_product
@@ -31,21 +32,22 @@ buttonSearchItem.addEventListener("click", function(e){
                 let totalItem_ = 0
                 totalItem_ = itens[i].val_max_product * quant;
                 newItem.totalItem = parseFloat(totalItem_).toFixed(2)
-    /*Rederiza na tela**/
-includedItems.innerHTML +=`
-        <div id="">
-            <span>${newItem.item}</span>
-            <span>${newItem.quant}</span>
-            <span>${newItem.valor}</span>
-            <span>${newItem.totalItem}</span>
-            <button onClick="">Apagar</button>
-        </ul>`
-    //console.log(newItem) 
-            soldItems.push(newItem)  
-            console.log(soldItems)
-        }})
-    })
-.catch(error =>console.log(error))
-}
+                includedItems.innerHTML +=`
+                <div id="parent">
+                <div>${newItem.item}</div>
+                <div id="x">${newItem.quant}</div>
+                <div>${newItem.valor}</div>
+                <div>${newItem.totalItem}</div>
+                <div><button onclick=myFunc()>Click me</button></div>
+                </div>
+                `
+                console.log(newItem)
+                soldItems.push(newItem)
+                }
+            }
+            )}).catch(error =>console.log(error))
+        }
 
-
+        function myFunc() {
+            $('#parent').empty();
+        }
